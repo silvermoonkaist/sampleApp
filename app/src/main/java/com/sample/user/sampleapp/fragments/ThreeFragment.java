04aeed9a65eb75f9099885a7de668d2e58b1d635
Sumin Han <hsm6911@gmail.com>
2015-12-26 19:08:22 +0900

    20151226 Git Sumin Testing firebase (currently not working)

--- app/src/main/java/com/sample/user/sampleapp/fragments/ThreeFragment.java
+++ app/src/main/java/com/sample/user/sampleapp/fragments/ThreeFragment.java
@@ -6,10 +6,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import android.app.ListActivity;
+import android.content.SharedPreferences;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.Random;
 import com.sample.user.sampleapp.R;
+import com.sample.user.sampleapp.chat.ChatListAdapter;
 
 public class ThreeFragment extends Fragment{
+    // TODO: change this to your own Firebase URL
+    private static final String FIREBASE_URL = "https://sampleappcs492.firebaseio.com/";
+
+    private String mUsername;
+    private Firebase mFirebaseRef;
+    private ValueEventListener mConnectedListener;
+    private ChatListAdapter mChatListAdapter;
+    private ListView listView;
 
     public ThreeFragment() {
         // Required empty public constructor
@@ -23,8 +51,75 @@ public class ThreeFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_one, container, false);
+        listView =  (ListView) rootView.findViewById(R.id.listView);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        return rootView;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
+        // Tell our list adapter that we only want 50 messages at a time
+        mChatListAdapter = new ChatListAdapter(mFirebaseRef.limit(50), this, R.layout.chat_message, mUsername);
+        listView.setAdapter(mChatListAdapter);
+        mChatListAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(mChatListAdapter.getCount() - 1);
+            }
+        });
+
+        // Finally, a little indication of connection status
+        mConnectedListener = mFirebaseRef.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean connected = (Boolean) dataSnapshot.getValue();
+                if (connected) {
+                    Toast.makeText(getActivity(), "Connected to Firebase", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "Disconnected from Firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                // No-op
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mFirebaseRef.getRoot().child(".info/connected").removeEventListener(mConnectedListener);
+        mChatListAdapter.cleanup();
+    }
+
+    private void setupUsername() {
+        SharedPreferences prefs = getApplication().getSharedPreferences("ChatPrefs", 0);
+        mUsername = prefs.getString("username", null);
+        if (mUsername == null) {
+            Random r = new Random();
+            // Assign a random user name if we don't have one saved.
+            mUsername = "JavaUser" + r.nextInt(100000);
+            prefs.edit().putString("username", mUsername).commit();
+        }
+    }
+
+    private void sendMessage() {
+        EditText inputText = (EditText) findViewById(R.id.messageInput);
+        String input = inputText.getText().toString();
+        if (!input.equals("")) {
+            // Create our 'model', a Chat object
+            Chat chat = new Chat(input, mUsername);
+            // Create a new, auto-generated child of that chat location, and save our chat data there
+            mFirebaseRef.push().setValue(chat);
+            inputText.setText("");
+        }
+    }
 }
