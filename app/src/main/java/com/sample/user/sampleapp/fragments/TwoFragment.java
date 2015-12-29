0a0656294adafd55c87a568f1ddc4271d5a2ae7a
EojinRho <djwls9453@gmail.com>
2015-12-29 18:12:12 +0900

    20151228 eojin

--- app/src/main/java/com/sample/user/sampleapp/fragments/TwoFragment.java
+++ app/src/main/java/com/sample/user/sampleapp/fragments/TwoFragment.java
@@ -4,8 +4,10 @@ package com.sample.user.sampleapp.fragments;
 import android.content.Context;
 import android.app.ListActivity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,9 +22,15 @@ import android.widget.Toast;
 
 import com.sample.user.sampleapp.R;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class TwoFragment extends Fragment{
+    ImageView imageview;
 
     public TwoFragment() {
         // Required empty public constructor
@@ -39,6 +47,21 @@ public class TwoFragment extends Fragment{
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_two, container, false);
         // Inflate the layout for this fragment
 
+        final SwipeRefreshLayout swipeView = (SwipeRefreshLayout) rootView.findViewById(R.id.swiperefresh);
+        imageview = (ImageView) rootView.findViewById(R.id.imageView3);
+
+        swipeView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                myUpdateOperation();
+            }
+        });
+
         return rootView;
+    }
+
+    protected void myUpdateOperation(){
+        imageview.setImageResource(R.drawable.piano);
+        Toast.makeText(getContext(),"updated!",Toast.LENGTH_SHORT).show();
     }
 }
